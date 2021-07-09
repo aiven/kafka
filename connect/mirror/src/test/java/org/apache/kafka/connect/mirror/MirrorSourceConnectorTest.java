d367b948088beed055b653ecc0b40f7d29dec94a
Ivan Yurchenko <ivanyu@aiven.io>
2021-07-09 17:01:03 +0300

    KAFKA-9914: Fix replication cycle detection with IdentityReplicationPolicy

--- connect/mirror/src/test/java/org/apache/kafka/connect/mirror/MirrorSourceConnectorTest.java
+++ connect/mirror/src/test/java/org/apache/kafka/connect/mirror/MirrorSourceConnectorTest.java
@@ -144,6 +144,7 @@ public class MirrorSourceConnectorTest {
         assertTrue(connector.shouldReplicateTopic("other.another.topic1"), "should allow anything else");
 
 
+
         final IdentityReplicationPolicy identityReplicationPolicy = new IdentityReplicationPolicy();
         final HashMap<String, String> props = new HashMap<>();
         props.put("source.cluster.alias", "source");
