85030cac3bd595de41a758d1944f927f9490e2ba
Kamal Chandraprakash <kchandraprakash@uber.com>
2021-11-26 15:27:27 +0530

    [LI-TIERING] Disable remote log storage on a topic dynamically
    Summary:
    We have to support disabling the dynamic remote log storage on a topic. This will be helpful to revert back to local-only storage in case of any issues/outages in the remote layer.
    
    Once the remote log storage gets disabled, the log-start-offset gets incremented to local-log-start-offset and the leader-epoch-checkpoint, producer-snapshots gets truncated up to the incremented offset. Also, the segments in remote log storage get deleted if it's available.
    
    Test Plan:
    - UT
    - IT
    
    Reviewers: prateeka, satishd, georgeli
    
    Reviewed By: prateeka

--- core/src/main/scala/kafka/log/UnifiedLog.scala
+++ core/src/main/scala/kafka/log/UnifiedLog.scala
@@ -212,6 +212,9 @@ case object SegmentDeletion extends LogStartOffsetIncrementReason {
 case object SnapshotGenerated extends LogStartOffsetIncrementReason {
   override def toString: String = "snapshot generated"
 }
+case object RemoteLogStorageDisabled extends LogStartOffsetIncrementReason {
+  override def toString: String = "remote log storage disabled"
+}
 
 /**
  * A log which presents a unified view of local and tiered log segments.
@@ -1119,6 +1122,10 @@ class UnifiedLog(@volatile var logStartOffset: Long,
 
   private def maybeIncrementLocalLogStartOffset(newLogStartOffset: Long, reason: LogStartOffsetIncrementReason): Unit = {
     maybeIncrementLogStartOffset(newLogStartOffset, reason, onlyLocalLogStartOffsetUpdate = true)
+  }
+
+  def maybeIncrementLogStartOffsetAsRemoteLogStorageDisabled(): Boolean = {
+    maybeIncrementLogStartOffset(localLogStartOffset, RemoteLogStorageDisabled)
   }
 
   /**
