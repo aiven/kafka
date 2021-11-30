620777e90aa9af5921a6113e086f659af430b74b
Kamal Chandraprakash <kchandraprakash@uber.com>
2021-11-30 11:17:00 +0530

    [LI-TIERING] Handle Stop Replica request in Remote Log Manager
    Summary:
    There are 3 cases in handling the stop replica request:
    - On broker shutdown: Stop the replica
    - On topic deletion: Stop the replica + delete the local log partition dir + delete the remote log partition dir
    - On partition shrink: Stop the replica + delete the local log partition dir
    
    Test Plan:
    - UT
    - Added two integration test
      # Topic Deletion test and
      # Replica Shrink test
    
    Reviewers: #ldap_kafka_admins, satishd, prateeka
    
    Reviewed By: #ldap_kafka_admins, satishd
    
    JIRA Issues: DKAFC-1573
    
    Differential Revision: https://code.uberinternal.com/D6888391

--- core/src/test/scala/unit/kafka/server/ReplicaManagerTest.scala
+++ core/src/test/scala/unit/kafka/server/ReplicaManagerTest.scala
@@ -28,6 +28,7 @@ import java.util.{Collections, Optional, Properties}
 import kafka.api._
 import kafka.cluster.{BrokerEndPoint, Partition}
 import kafka.log._
+import kafka.log.remote.RemoteLogManager
 import kafka.server.QuotaFactory.{QuotaManagers, UnboundedQuota}
 import kafka.server.checkpoints.{LazyOffsetCheckpoints, OffsetCheckpointFile}
 import kafka.server.epoch.util.ReplicaFetcherMockBlockingSend
@@ -70,6 +71,7 @@ class ReplicaManagerTest {
   var alterIsrManager: AlterIsrManager = _
   var config: KafkaConfig = _
   var quotaManager: QuotaManagers = _
+  var remoteLogManager: RemoteLogManager = _
 
   // Constants defined for readability
   val zkVersion = 0
@@ -82,6 +84,7 @@ class ReplicaManagerTest {
     val props = TestUtils.createBrokerConfig(1, TestUtils.MockZkConnect)
     config = KafkaConfig.fromProps(props)
     alterIsrManager = EasyMock.createMock(classOf[AlterIsrManager])
+    remoteLogManager = EasyMock.createMock(classOf[RemoteLogManager])
     quotaManager = QuotaFactory.instantiate(config, metrics, time, "")
   }
 
@@ -1838,7 +1841,7 @@ class ReplicaManagerTest {
     val mockDelayedElectLeaderPurgatory = new DelayedOperationPurgatory[DelayedElectLeader](
       purgatoryName = "DelayedElectLeader", timer, reaperEnabled = false)
 
-    new ReplicaManager(config, metrics, time, None, scheduler, mockLogMgr, None,
+    new ReplicaManager(config, metrics, time, None, scheduler, mockLogMgr, Some(remoteLogManager),
       new AtomicBoolean(false), quotaManager, new BrokerTopicStats,
       metadataCache, new LogDirFailureChannel(config.logDirs.size), mockProducePurgatory, mockFetchPurgatory,
       mockDeleteRecordsPurgatory, mockDelayedElectLeaderPurgatory, mockDelayedRemoteFetchPurgatory, Option(this.getClass.getName),
@@ -2083,14 +2086,18 @@ class ReplicaManagerTest {
 
     val tp0 = new TopicPartition(topic, 0)
     val offsetCheckpoints = new LazyOffsetCheckpoints(replicaManager.highWatermarkCheckpoints)
-    replicaManager.createPartition(tp0).createLogIfNotExists(isNew = false, isFutureReplica = false, offsetCheckpoints, None)
+    val partition = replicaManager.createPartition(tp0)
+    partition.createLogIfNotExists(isNew = false, isFutureReplica = false, offsetCheckpoints, None)
 
+    val topicIds = Collections.singletonMap(topic, Uuid.randomUuid())
     val becomeLeaderRequest = new LeaderAndIsrRequest.Builder(ApiKeys.LEADER_AND_ISR.latestVersion, 0, 10, brokerEpoch,
       Seq(leaderAndIsrPartitionState(tp0, 1, 0, Seq(0, 1), true)).asJava,
-      Collections.singletonMap(topic, Uuid.randomUuid()),
+      topicIds,
       Set(new Node(0, "host1", 0), new Node(1, "host2", 1)).asJava
     ).build()
 
+    EasyMock.expect(remoteLogManager.onLeadershipChange(Set(partition), Set(), topicIds))
+    EasyMock.replay(remoteLogManager)
     replicaManager.becomeLeaderOrFollower(1, becomeLeaderRequest, (_, _) => ())
 
     val partitionStates = Map(tp0 -> new StopReplicaPartitionState()
@@ -2101,6 +2108,7 @@ class ReplicaManagerTest {
 
     val (_, error) = replicaManager.stopReplicas(1, 0, 0, partitionStates)
     assertEquals(Errors.STALE_CONTROLLER_EPOCH, error)
+    EasyMock.verify(remoteLogManager)
   }
 
   @Test
@@ -2110,14 +2118,18 @@ class ReplicaManagerTest {
 
     val tp0 = new TopicPartition(topic, 0)
     val offsetCheckpoints = new LazyOffsetCheckpoints(replicaManager.highWatermarkCheckpoints)
-    replicaManager.createPartition(tp0).createLogIfNotExists(isNew = false, isFutureReplica = false, offsetCheckpoints, None)
+    val partition = replicaManager.createPartition(tp0)
+    partition.createLogIfNotExists(isNew = false, isFutureReplica = false, offsetCheckpoints, None)
 
+    val topicIds = Collections.singletonMap(topic, Uuid.randomUuid())
     val becomeLeaderRequest = new LeaderAndIsrRequest.Builder(ApiKeys.LEADER_AND_ISR.latestVersion, 0, 0, brokerEpoch,
       Seq(leaderAndIsrPartitionState(tp0, 1, 0, Seq(0, 1), true)).asJava,
-      Collections.singletonMap(topic, Uuid.randomUuid()),
+      topicIds,
       Set(new Node(0, "host1", 0), new Node(1, "host2", 1)).asJava
     ).build()
 
+    EasyMock.expect(remoteLogManager.onLeadershipChange(Set(partition), Set(), topicIds))
+    EasyMock.replay(remoteLogManager)
     replicaManager.becomeLeaderOrFollower(1, becomeLeaderRequest, (_, _) => ())
     replicaManager.markPartitionOffline(tp0)
 
@@ -2130,6 +2142,7 @@ class ReplicaManagerTest {
     val (result, error) = replicaManager.stopReplicas(1, 0, 0, partitionStates)
     assertEquals(Errors.NONE, error)
     assertEquals(Map(tp0 -> Errors.KAFKA_STORAGE_ERROR), result)
+    EasyMock.verify(remoteLogManager)
   }
 
   @Test
@@ -2167,6 +2180,8 @@ class ReplicaManagerTest {
       .setDeletePartition(deletePartitions)
     )
 
+    EasyMock.expect(remoteLogManager.stopPartitions(EasyMock.eq(Set(tp0)), EasyMock.eq(false), EasyMock.anyObject())).once()
+    EasyMock.replay(remoteLogManager)
     val (result, error) = replicaManager.stopReplicas(1, 0, 0, partitionStates)
     assertEquals(Errors.NONE, error)
 
@@ -2180,6 +2195,7 @@ class ReplicaManagerTest {
       assertEquals(Map(tp0 -> Errors.NONE), result)
       assertTrue(replicaManager.logManager.getLog(tp0).isDefined)
     }
+    EasyMock.verify(remoteLogManager)
   }
 
   @Test
@@ -2262,11 +2278,19 @@ class ReplicaManagerTest {
         logDirFailureChannel).read()
     }
 
+    val topicIds = Collections.singletonMap(tp0.topic(), Uuid.randomUuid())
     val becomeLeaderRequest = new LeaderAndIsrRequest.Builder(ApiKeys.LEADER_AND_ISR.latestVersion, 0, 0, brokerEpoch,
       Seq(leaderAndIsrPartitionState(tp0, 1, 0, Seq(0, 1), true)).asJava,
-      Collections.singletonMap(tp0.topic(), Uuid.randomUuid()),
+      topicIds,
       Set(new Node(0, "host1", 0), new Node(1, "host2", 1)).asJava
     ).build()
+
+    EasyMock.expect(remoteLogManager.onLeadershipChange(Set(partition), Set(), topicIds))
+    if (expectedOutput == Errors.NONE || throwIOException) {
+      val delete = deletePartition && leaderEpoch == LeaderAndIsr.EpochDuringDelete
+      EasyMock.expect(remoteLogManager.stopPartitions(EasyMock.eq(Set(tp0)), EasyMock.eq(delete), EasyMock.anyObject())).once()
+    }
+    EasyMock.replay(remoteLogManager)
 
     replicaManager.becomeLeaderOrFollower(1, becomeLeaderRequest, (_, _) => ())
 
@@ -2303,6 +2327,7 @@ class ReplicaManagerTest {
       assertFalse(readRecoveryPointCheckpoint().contains(tp0))
       assertFalse(readLogStartOffsetCheckpoint().contains(tp0))
     }
+    EasyMock.verify(remoteLogManager)
   }
 
   @Test
