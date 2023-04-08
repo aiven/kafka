323b4c8af570a8600aca4e228d3ffe67378a0fad
Satish Duggana <satishd@apache.org>
2023-04-08 15:14:58 +0530

    Added remote log segments retention functionality based on time and size  - RLMTask runs retention cleanup activity in each execution for the targeted topic partition. It checks for total size, retention time and deletes the eligible segments from remote storage. It also updates the log-start-offset in UnifiedLog.

--- core/src/main/scala/kafka/server/BrokerServer.scala
+++ core/src/main/scala/kafka/server/BrokerServer.scala
@@ -529,7 +529,12 @@ class BrokerServer(
       }
 
       Some(new RemoteLogManager(config.remoteLogManagerConfig, config.brokerId, config.logDirs.head, clusterId, time,
-        (tp: TopicPartition) => logManager.getLog(tp).asJava));
+        (tp: TopicPartition) => logManager.getLog(tp).asJava,
+        (tp: TopicPartition, remoteLogStartOffset: java.lang.Long) => {
+          logManager.getLog(tp).foreach(log => {
+            log.updateLogStartOffsetFromRemoteTier(remoteLogStartOffset)
+          })
+        }));
     } else {
       None
     }
