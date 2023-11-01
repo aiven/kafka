ab7ac0fd7d7003d8cfefa3286d845494c58d7956
Alok Thatikunta <athatikunta@confluent.io>
2023-11-01 22:10:45 +0530

    KAFKA-14484: [1/N] Move PartitionMetadataFile to storage module (#14607)
    This PR moves PartitionMetadataFile to the storage module.
    
    Existing unit tests in UnifiedLogTest like testLogFlushesPartitionMetadataOnAppend should suffice.
    
    Reviewers: Ismael Juma <ismael@juma.me.uk>, Jun Rao <junrao@gmail.com>

--- core/src/test/scala/unit/kafka/server/ReplicaManagerTest.scala
+++ core/src/test/scala/unit/kafka/server/ReplicaManagerTest.scala
@@ -74,6 +74,7 @@ import org.apache.kafka.common.message.MetadataResponseData.{MetadataResponsePar
 import org.apache.kafka.raft.RaftConfig
 import org.apache.kafka.server.log.remote.storage.{NoOpRemoteLogMetadataManager, NoOpRemoteStorageManager, RemoteLogManagerConfig}
 import org.apache.kafka.server.util.timer.MockTimer
+import org.apache.kafka.storage.internals.checkpoint.PartitionMetadataFile
 import org.mockito.invocation.InvocationOnMock
 import org.mockito.stubbing.Answer
 import org.mockito.{ArgumentCaptor, ArgumentMatchers}
