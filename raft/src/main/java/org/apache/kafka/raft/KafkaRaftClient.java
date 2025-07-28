547996c52e9b5ad40d20a1ed4ede0b66bb938cdb
Alyssa Huang <ahuang@confluent.io>
2025-07-28 10:03:14 -0700

    KAFKA-19354: KRaft observer should send fetch to best node (#19854)
    Observers may get stuck fetching from bootstrap servers even on
    discovery of a leader from a fetch response.
    
    Observers currently fetch from bootstrap servers once their fetch
    timeout expires, and even on rediscovery of the leader (e.g. observer
    receives fetch response indicating location of leader), observers will
    not  attempt to resume fetching from the leader.
    
    This change simplifies observer polling logic to just rely on
    maybeSendFetchToBestNode which takes care of the logic of selecting
    either the leader or bootstrap servers  as the destination based on
    timeouts and backoffs.
    
    Reviewers: José Armando García Sancio <jsancio@apache.org>

--- raft/src/main/java/org/apache/kafka/raft/KafkaRaftClient.java
+++ raft/src/main/java/org/apache/kafka/raft/KafkaRaftClient.java
@@ -2548,7 +2548,7 @@ public final class KafkaRaftClient<T> implements RaftClient<T> {
                 transitionToUnattached(epoch, OptionalInt.empty());
             }
         } else if (
-                leaderId.isPresent() &&
+            leaderId.isPresent() &&
                 (!quorum.hasLeader() || leaderEndpoints.size() > quorum.leaderEndpoints().size())
         ) {
             // The request or response indicates the leader of the current epoch
@@ -3217,11 +3217,7 @@ public final class KafkaRaftClient<T> implements RaftClient<T> {
     }
 
     private long pollFollowerAsObserver(FollowerState state, long currentTimeMs) {
-        if (state.hasFetchTimeoutExpired(currentTimeMs)) {
-            return maybeSendFetchToAnyBootstrap(currentTimeMs);
-        } else {
-            return maybeSendFetchToBestNode(state, currentTimeMs);
-        }
+        return maybeSendFetchToBestNode(state, currentTimeMs);
     }
 
     private long maybeSendFetchToBestNode(FollowerState state, long currentTimeMs) {
