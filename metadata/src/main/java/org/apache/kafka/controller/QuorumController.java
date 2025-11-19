4b97508d412f4bec4e395f4316b5afd351393e6d
Ivan Yurchenko <ivanyu@aiven.io>
2025-11-19 13:58:25 +0100

    [MINOR] Add Aiven policies
    This commit adds support for Aiven policies.

--- metadata/src/main/java/org/apache/kafka/controller/QuorumController.java
+++ metadata/src/main/java/org/apache/kafka/controller/QuorumController.java
@@ -1621,6 +1621,9 @@ public final class QuorumController implements Controller {
             setSnapshotRegistry(snapshotRegistry).
             setClusterControlManager(clusterControl).
             build();
+
+        AivenTopicPolicy aivenTopicPolicy = new AivenTopicPolicy();
+        aivenTopicPolicy.configure(staticConfig);
         this.replicationControl = new ReplicationControlManager.Builder().
             setSnapshotRegistry(snapshotRegistry).
             setLogContext(logContext).
@@ -1639,6 +1642,7 @@ public final class QuorumController implements Controller {
             setClusterControl(clusterControl).
             setCreateTopicPolicy(createTopicPolicy).
             setFeatureControl(featureControl).
+            setAivenTopicPolicy(aivenTopicPolicy).
             build();
         this.scramControlManager = new ScramControlManager.Builder().
             setLogContext(logContext).
