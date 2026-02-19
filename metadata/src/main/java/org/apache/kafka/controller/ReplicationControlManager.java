19f5746d6040e89689159f77ecafb68697ae8634
Guilherme Quentel Melo <guilhermequentel.melo@aiven.io>
2026-02-19 16:39:58 +0100

    MINOR: add max replication factor to Aiven policies

--- metadata/src/main/java/org/apache/kafka/controller/ReplicationControlManager.java
+++ metadata/src/main/java/org/apache/kafka/controller/ReplicationControlManager.java
@@ -905,7 +905,7 @@ public class ReplicationControlManager {
                 }
             }
             if (aivenTopicPolicy != null) {
-                ApiError aivenPolicyError = aivenTopicPolicy.validateTopicCreation(topic.name(), newParts.size(), topics);
+                ApiError aivenPolicyError = aivenTopicPolicy.validateTopicCreation(topic, newParts.size(), topics);
                 if (aivenPolicyError.isFailure()) return aivenPolicyError;
             }
             ApiError error = maybeCheckCreateTopicPolicy(() -> {
@@ -930,7 +930,7 @@ public class ReplicationControlManager {
             short disklessReplicationFactor = isDisklessManagedReplicasEnabled ? classicReplicationFactor : 1;
             short replicationFactor = disklessEnabled ? disklessReplicationFactor : classicReplicationFactor;
             if (aivenTopicPolicy != null) {
-                ApiError aivenPolicyError = aivenTopicPolicy.validateTopicCreation(topic.name(), numPartitions, topics);
+                ApiError aivenPolicyError = aivenTopicPolicy.validateTopicCreation(topic, numPartitions, topics);
                 if (aivenPolicyError.isFailure()) return aivenPolicyError;
             }
             try {
